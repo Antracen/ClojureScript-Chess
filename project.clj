f9115f8be67399dbafc54a40fa7cf0fe1f50dc43
Antracen <martin.rr.wass@gmail.com>
2023-02-13 21:46:16 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
  :dependencies [[re-frame "1.3.0"]
                 [reagent "1.1.1"]
                 [org.clojars.rutledgepaulv/websocket-fx "0.1.2"]]
+ :min-lein-version "2.0.0"
  :build-defaults {:build-hooks [(shadow-git-inject.core/hook)]}
  :dev-http {8280 "resources/public"}
  :builds {:reframe {:target :browser
